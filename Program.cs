f55783037b43591ab4547f55636b18f9d9b3ed44
Vladislav Churilov <Nawaharjan@gmail.com>
2024-07-18 14:53:40 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraySum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 5, 8, 6, 9 };
+            int maxElement = int.MinValue;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > maxElement)
+                {
+                    maxElement = array[i];
+                    Console.WriteLine(maxElement);
+                }
+            }
+        }
+    }
+}
